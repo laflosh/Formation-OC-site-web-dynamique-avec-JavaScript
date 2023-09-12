cd75c59028b3029e5cacfc26bb601e37c05ce05f
laflosh <l_florent_l@hotmail.fr>
2023-09-12 13:31:40 +0200

    Derniers ajouts

--- pieces.js
+++ pieces.js
@@ -153,3 +153,9 @@ function listeDisponible(){
 
     document.querySelector('.disponibles').appendChild(disponiblesElement);
 }
+
+// Calcul du nombre total de commentaires par quantité d'étoiles attribuées
+const avis = await fetch("http://localhost:8081/avis").then(avis => avis.json());
+const nb_commentaires = [0, 0, 0, 0, 0];
+for (let commentaire of avis) {
+  nb_commentaires[commentaire.nbEtoiles - 1]++;
